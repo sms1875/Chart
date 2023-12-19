593ab7393c9dc306f87cb82b75c3dc5df4e0f9a2
송명석 <01audtjr@gmail.com>
2023-12-19 15:54:04 +0900

    주석 추가

--- src/pages/survey/SurveyChart.js
+++ src/pages/survey/SurveyChart.js
@@ -20,7 +20,14 @@ ChartJS.register(
   annotationPlugin
 );
 
-// Y축의 옵션 설정
+/**
+ * Y 축의 옵션을 설정하는 함수
+ * @param {string} axisType - 축의 유형
+ * @param {boolean} display - 표시 여부
+ * @param {string} position - 위치
+ * @param {string} axisLabel - 축 레이블
+ * @returns {Object} - Y 축의 옵션
+ */
 const getYAxisOptions = (axisType, display, position, axisLabel) => ({
   type: axisType,
   display,
@@ -38,7 +45,13 @@ const getYAxisOptions = (axisType, display, position, axisLabel) => ({
   axisLabel,
 });
 
-// 차트 데이터 생성
+/**
+ * 차트 데이터셋을 생성하는 함수
+ * @param {Array} selectedAxes - 선택된 Y 축
+ * @param {Array} chartData - 차트 데이터
+ * @param {Array} colors - 색상 배열
+ * @returns {Array} - 생성된 차트 데이터셋 배열
+ */
 const generateChartDataSets = (selectedAxes, chartData, colors) => {
   return chartData.map((data, index) => {
     const isDataSelected = selectedAxes.includes(data.axis);
@@ -53,7 +66,15 @@ const generateChartDataSets = (selectedAxes, chartData, colors) => {
   }).filter(Boolean);
 };
 
-// 차트 옵션 생성
+/**
+ * 차트 옵션을 생성하는 함수
+ * @param {Array} selectedAxes - 선택된 Y 축
+ * @param {Object} axisConfig - Y 축 설정
+ * @param {number} xMin - X 최소값
+ * @param {number} xMax - X 최대값
+ * @param {boolean} isAnnotationEnabled - 주석 활성화 여부
+ * @returns {Object} - 차트 옵션
+ */
 const getChartOptions = (selectedAxes, axisConfig, xMin, xMax, isAnnotationEnabled) => ({
   scales: {
     x: {
@@ -89,20 +110,30 @@ const getChartOptions = (selectedAxes, axisConfig, xMin, xMax, isAnnotationEnabl
           type: 'box',
           xMin,
           xMax,
-          borderColor: 'gray',
+          backgroundColor: 'yellow',
         },
       },
     },
   },
 });
 
-// 색상 배열 생성
+/**
+ * 색상 배열을 생성하는 함수
+ * @param {number} count - 색상 개수
+ * @returns {Array} - 생성된 색상 배열
+ */
 const generateRandomColors = (count) => {
   return Array.from({ length: count }, () => '#' + Math.floor(Math.random() * 16777215).toString(16));
 };
 
-const colors = generateRandomColors(100);
-// 주석 및 재사용 가능한 요소들을 포함한 차트 컴포넌트
+const colors = generateRandomColors(100); // 색상 배열
+
+/**
+ * 설문 결과를 표시하는 차트 컴포넌트
+ * @param {Object} props - 컴포넌트 속성
+ * @param {Object} props.ChartItem - 차트 데이터 및 설정
+ * @returns {JSX.Element} - 렌더링된 설문 결과 차트 컴포넌트
+ */
 const SurveyChart = ({ ChartItem }) => {
   // State 정의
   const [selectedAxes, setSelectedAxes] = useState([ChartItem.axis[0]]); // 선택된 Y 축
@@ -111,8 +142,10 @@ const SurveyChart = ({ ChartItem }) => {
   const [isAnnotationEnabled, setIsAnnotationEnabled] = useState(false); // 주석 활성화 여부
 
   const maxSelectedAxes = 3; // 선택 가능한 최대 Y 축 수
-
-  // Y 축 선택 핸들러
+  /**
+   * Y 축 선택 핸들러
+   * @param {string} axis - 선택된 Y 축
+   */
   const handleAxisToggle = (axis) => {
     setSelectedAxes((prevAxes) => {
       if (prevAxes.includes(axis)) {
@@ -124,14 +157,16 @@ const SurveyChart = ({ ChartItem }) => {
     });
   };
 
-  // 주석 활성화 토글 핸들러
+  /**
+   * 주석 활성화 토글 핸들러
+   */
   const handleAnnotationToggle = () => {
     setIsAnnotationEnabled((prevValue) => !prevValue);
   };
 
   // 차트 옵션 설정
   const axisConfig = ChartItem.axis.reduce((axesConfig, axis) => {
-    axesConfig[axis] = getYAxisOptions('linear', selectedAxes.includes(axis), selectedAxes.length > 0 && selectedAxes[0] === axis ? 'left' : 'right', `${axis} Axis Label`);
+    axesConfig[axis] = getYAxisOptions('linear', selectedAxes.includes(axis), selectedAxes.length > 0 && selectedAxes[0] === axis ? 'left' : 'right', `${axis} 축`);
     return axesConfig;
   }, {});
 
@@ -156,9 +191,9 @@ const SurveyChart = ({ ChartItem }) => {
             <input type="checkbox" checked={isAnnotationEnabled} onChange={handleAnnotationToggle} />
             주석 활성화
           </label>
-          <label htmlFor="xMinInput">X Min:</label>
+          <label htmlFor="xMinInput">X 최소값:</label>
           <input type="number" id="xMinInput" value={xMin} onChange={(e) => setXMin(e.target.value)} />
-          <label htmlFor="xMaxInput">X Max:</label>
+          <label htmlFor="xMaxInput">X 최대값:</label>
           <input type="number" id="xMaxInput" value={xMax} onChange={(e) => setXMax(e.target.value)} />
         </div>
       </div>
